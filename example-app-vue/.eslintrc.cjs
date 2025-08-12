cd114603982eb501c6c97322f55527058acbdea4
WcaleNieWolny <isupermichael007@gmail.com>
2025-08-12 13:54:03 +0100

    fix: lint

--- example-app-vue/.eslintrc.cjs
+++ example-app-vue/.eslintrc.cjs
@@ -4,9 +4,8 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/recommended',
+    'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
   ],
   parserOptions: {
     ecmaVersion: 2020
